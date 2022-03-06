82001d8ff29ff2707100c1579cfcabb39082455e
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-03-06 15:12:18 +0300

    Главная функция алгоритма

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function write_mas(title, mas){
+    document.write(title)
+    for (let i = 0; i<mas.length;i++){
+        document.write(mas[i].join(' '))
+        document.write("<br>")
+    }
+}
+
+function main(){
+    //ввод эталонных данных
+    var flag_Xk = 0 // 0 - из программы, 1 - из базы данных
+    if (flag_Xk == 0){
+        //функция передачи данных из программы
+        var Xk_var = Xk([])
+        write_mas("Эталонные значения:<br>", Xk_var);
+    }
+    console.log(Xk_var)
+    //поиск максимум минимум
+    var max_var = max(Xk_var)
+    var min_var = min(Xk_var)
+    //нормализация центров кластеров
+        //на выходи двумерная матрица
+    var norm_Xk_var = norm_Xk(Xk_var, max_var, min_var, [])
+    console.log(norm_Xk_var)
+    write_mas("Нормированные эталонные значения:<br>", norm_Xk_var)
+    //ввод данных об успеваемости
+        //одномерный массив успеваемости одного сутдента по всем параметрам
+    var flag_x = 0;
+    if (flag_x == 0){
+        var x_var = x([]);
+    }
+    //нормализация
+    
+    //расстояние
+
+    //определение класса
+
+    
+}
+
+console.log("File main.js included")
+main()
