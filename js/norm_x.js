2d3e668809f7a8ccb0d2313b06e31ba9ca126c0f
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-03-06 15:13:23 +0300

    Функция нормирования одного объекта

---
+++ js/norm_x.js
@@ -0,0 +1,8 @@
+//вход: одномерный массив, int, int, одномерный массив
+//выход: одномерный массив
+function norm_x(mas, max, min, rez){
+    for (let i = 0; i<mas.length; i++){
+        rez.push(object_rationing(mas[i], max, min));
+    }
+    return rez;
+}
