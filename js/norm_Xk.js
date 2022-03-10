6f1353b835ec0dc84da649a82b0cad8471bba1fd
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-03-10 11:38:36 +0300

    Добавление требуемых файлов и переписывание аогоритма нормировки двумерной матрицы

--- js/norm_Xk.js
+++ js/norm_Xk.js
@@ -1,15 +1,10 @@
+//Требуемыей файлы: norm_x.js
+
 function norm_Xk(Xk_var, max, min, norm_Xk){
-    var m = [];
-    for (let i = 0; i<Xk_var.length; i++){
-        norm_Xk = get(norm_Xk, m);
-        for (let j = 0; j<Xk_var[i].length; j++){
-            norm_Xk[i][j] = object_rationing(Xk_var[i][j], max, min);
-        }
-        //console.log(m)
-        
-        console.log(norm_Xk)
-        //console.log(m.join(' '))
-        m.splice(0, m.length);
+    len_Xk_var = Xk_var.length
+    for (let i = 0; i< len_Xk_var; i++)
+    {
+        norm_Xk.push(norm_x(Xk_var[i], max, min, []));
     }
     return norm_Xk;
 }
