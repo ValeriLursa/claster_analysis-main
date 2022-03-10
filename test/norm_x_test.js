2ff8e34e4b3df92877af4334c52ba1b92c9e65d0
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-03-10 11:42:07 +0300

    Выделение сравнения двух массивов в отдельный файл

--- test/norm_x_test.js
+++ test/norm_x_test.js
@@ -1,27 +1,4 @@
-//Требуются файлы norm_x, object_rationing.js, test/mess.js
-
-function equal_arrays(mas1, mas2){
-    if (mas1.length != mas2.length ) return 1;
-    if (mas1.length + mas2.length == 0) return 0;
-    if (mas1[0] != mas2[0]) return 1;
-        else return equal_arrays(mas1.splice(1), mas2.splice(1));
-}
-
-function equal_arrays_test(){
-    document.write("Тестирование функции equal_arrays<br>")
-    mas1 = [1, 2, 3]
-    mas2 = [1, 2, 3]
-    if (equal_arrays(mas1, mas2) == 0) mess(1, "true"); else mess(1, "false");
-    mas1 = []
-    mas2 = []
-    if (equal_arrays(mas1, mas2) == 0) mess(2, "true"); else mess(1, "false");
-    mas1 = []
-    mas2 = [1]
-    if (equal_arrays(mas1, mas2) == 1) mess(3, "true"); else mess(1, "false");
-    mas1 = [2, 2, 3]
-    mas2 = [1, 2, 3]
-    if (equal_arrays(mas1, mas2) == 1) mess(4, "true"); else mess(1, "false");
-}
+//Требуются файлы norm_x, object_rationing.js, test/mess.js, test/equal_arrays.js
 
 function norm_x_test(){
     document.write("Тестирование функции norm_x<br>")
