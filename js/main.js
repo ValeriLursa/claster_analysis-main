12676682064aa3b430e7fb17934859decbe46b47
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-03-10 11:37:41 +0300

    Добавление требуемых файлов и исправление ошибок в комментарии

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Требуемые файлы Xk.js, 
+
 function write_mas(title, mas){
     document.write(title)
     for (let i = 0; i<mas.length;i++){
@@ -19,7 +21,7 @@ function main(){
     var max_var = max(Xk_var)
     var min_var = min(Xk_var)
     //нормализация центров кластеров
-        //на выходи двумерная матрица
+        //на выходе двумерная матрица
     var norm_Xk_var = norm_Xk(Xk_var, max_var, min_var, [])
     console.log(norm_Xk_var)
     write_mas("Нормированные эталонные значения:<br>", norm_Xk_var)
