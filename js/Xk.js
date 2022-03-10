fe3bb3c17d4fbee57fd5dc98b3454481ea41f868
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-03-10 11:40:22 +0300

    Изменение алгоритма ввода тестовых значений кластеров

--- js/Xk.js
+++ js/Xk.js
@@ -1,13 +1,15 @@
+//Ввод эталонных данных из программы
+
 function get(Xk, mas){
     Xk.push(mas);
     return Xk;
 }
 
 function Xk(Xk_var){
-    Xk_var = get(Xk_var, [100, 0, 0]);
-    Xk_var = get(Xk_var, [74.5, 3, 3]);
-    Xk_var = get(Xk_var, [25, 5, 5]);
-    Xk_var = get(Xk_var, [0, 10, 10]);
+    Xk_var = push([100, 0, 0]);
+    Xk_var = push([74.5, 3, 3]);
+    Xk_var = push([25, 5, 5]);
+    Xk_var = push([0, 10, 10]);
     return Xk_var;
 }
 
