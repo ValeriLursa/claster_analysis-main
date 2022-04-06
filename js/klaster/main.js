57a32ac327b1abe4a41e6b27927fdfbaf36e6e30
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-04-06 17:25:46 +0300

    Добавление функции для кластеризации данных с формы

--- js/klaster/main.js
+++ js/klaster/main.js
@@ -6,10 +6,8 @@ function write_mas(title, mas){
     if (typeof mas == "number"){
         str += mas + '<br>'
     }
-    console.log(mas)
     if (mas instanceof Array) {
         var len_array = mas[0].length
-        console.log(mas[0].length)
         if (typeof len_array === 'undefined')
         {
             //одномерный массив
@@ -65,3 +63,75 @@ function main(){
 
 console.log("File main.js included")
 //main()
+
+function getXk(col){
+    var q = 0.1 //10% для отлично, 10% для средне, 80% плохо
+    var Xk_var = []
+    if (col == 1){
+        //если только одно поле для проверки, то два кластера
+        Xk_var = get(Xk_var, [1, 2])
+        Xk_var = get(Xk_var, [0, 10])
+    }
+    else {
+        //если больше одного, три кластера
+        var qq = col - (col*q/2) //центр первого кластера, положительного
+        var qn = col / 6
+        Xk_var = get(Xk_var, [Math.floor(qq)])
+        col -= col*q //меняю верхнюю границу
+        qq = col - (col*q/2) //центра второго кластера, среднего
+        qn += (col/3)
+        Xk_var = get(Xk_var, [Math.floor(qq)])
+        col -= col*q //меняю верхнюю границу
+        qq = col - (col/2) //центр третьего кластера, плохого
+        qn += (col/3)
+        Xk_var = get(Xk_var, [qq])
+    }
+    return Xk_var
+}
+
+function referenceData(col){
+    
+    return norm_Xk_var;
+}
+
+function clustering(result, col){
+    //Расчет эталонных значений
+    var Xk_var = getXk(col)
+    console.log(Xk_var)
+    //Просчет макс мин
+    var max_var = max(Xk_var)
+    var min_var = min(Xk_var)
+    //нормальизация эталонных данных
+    var norm_Xk_var = norm_Xk(Xk_var, max_var, min_var, [])
+    console.log(result)
+    //нормализация
+    var norm_x_var = norm_x(result, max_var, min_var, [])
+    //расстояние
+    var euc_dist_var = []
+    for (let i=0;i<norm_Xk_var.length;i++){
+        euc_dist_var.push(euc_distance(norm_Xk_var[i], norm_x_var, 0))
+    }
+    console.log(euc_dist_var)
+    //определение класса
+    var q = klaster(euc_dist_var)
+    console.log(q)
+    //Сообщение
+    var mes = messageClustering(col, q)
+    return mes
+}
+
+function messageClustering(col, clust){
+    if (col == 1){
+        switch(clust){
+            case 1: return "Хорошо"
+            case 2: return "Плохо"
+        }
+    }
+    else{
+        switch(clust){
+            case 1: return "Хорошо"
+            case 2: return "Средне"
+            case 3: return "Плохо"
+        }
+    }
+}
