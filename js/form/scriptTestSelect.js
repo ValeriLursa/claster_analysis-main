5e8b3c739832bcdbb34faf1437e377b253f79918
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-04-06 17:25:17 +0300

    Выделение проверки задания с селектами в отдельную функцию

--- js/form/scriptTestSelect.js
+++ js/form/scriptTestSelect.js
@@ -1,3 +1,39 @@
+var resultCheck = [];
+var resultClustering = [];
+var numberEndTry = 0;
+
+function checkSelect(s){
+	//проверка заданий с селектами
+	numberEndTry++;
+	var str = "sel" + s;
+	var ss = "select" + s;
+	var wordProv = document.getElementsByClassName(str);
+	len = wordProv.length
+	//проверка задания
+	resultCheck.push([clickSelect(wordProv, masProvSelect[s])])
+	//кластеризация
+	var rezultq = clustering(resultCheck[numberEndTry - 1], len);
+	//добавление резльтата кластеризации
+	resultClustering.push(rezultq)
+	console.log(rezultq)
+	//отметка правильности выполнения задания
+	if (rezultq == "Плохо") document.getElementById(ss).className = "w3-bar-item w3-button tablink w3-red";
+	if (rezultq == "Средне") document.getElementById(ss).className = "w3-bar-item w3-button tablink w3-yellow";
+	if (rezultq == "Хорошо") document.getElementById(ss).className = "w3-bar-item w3-button tablink w3-green";
+	//Вывод резльтатов всех попыток
+	var print = "printSelect" + s
+	var printBlock = document.getElementById(print);
+	var strColorWord = "<tr><td>Номер попытки</td><td>Результат</td><td>Подсказка</td></tr>"
+	var len = resultCheck.length; 
+	for (var i = 0; i < len; i++){
+		strColorWord += "<tr> <td>"
+		strColorWord += (i+1).toString() + "</td>"
+		strColorWord += "<td>" + resultCheck[i][0].toString() + "</td>"
+		strColorWord += "<td>" + resultClustering[i].toString() + "</td>"
+	}
+	printBlock.innerHTML = strColorWord
+}
+
 
 function selIndex(ii,masRez) {
 	var selectStr = "<select size=\"1\" class=\"sel" + ii + "\"> <option>-</option>";
@@ -9,12 +45,11 @@ function selIndex(ii,masRez) {
 }
 
 function clickSelect(array, masRez) {
-	var selekt = true;
-	console.log(array.length);
+	var selekt = array.length;
 	for (var i = 0; i < array.length; i++) {
 		if (array[i].options[array[i].selectedIndex].value != masRez[i]) {
 			array[i].value = "-";
-			selekt = false;
+			selekt--;
 		}
 	}
 	return selekt;
