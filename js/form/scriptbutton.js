6767f46668f533d0c57ed55bec7add007033e8af
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-04-07 16:51:41 +0300

    Поправление интерфейса пользователя

--- js/form/scriptbutton.js
+++ js/form/scriptbutton.js
@@ -1,17 +1,29 @@
+var heightH = 0
+
 jQuery(document).ready(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
     $(window).scroll(function () {
         var height = $(window).scrollTop();
-        if (height > 100) {
+        if (height > 500) {
             $('#back2Top').fadeIn();
+            $('#back2Top2').fadeOut();
         } else {
             $('#back2Top').fadeOut();
+            $('#back2Top2').fadeIn();
         }
     });
-    
         $("#back2Top").click(function (event) {
             event.preventDefault();
             $("html, body").animate({ scrollTop: 0 }, "slow");
             return false;
         });
+
+        $("#back2Top2").click(function (event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: document.documentElement.scrollHeight }, "slow");
+            return false;
+        });
     
     });
+
+console.log(document.documentElement.scrollHeight)
